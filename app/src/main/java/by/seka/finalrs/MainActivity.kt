1e833174fbab4731866a63c3c6e22fab456d1a06
borisyonok_a <borisyonok_a@lwo.by>
2022-11-11 11:43:54 +0300

    Initial commit

---
+++ app/src/main/java/by/seka/finalrs/MainActivity.kt
@@ -0,0 +1,11 @@
+package by.seka.finalrs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
